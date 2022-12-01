0d705e2e8cc722227f4815360a9080ac05ab6235
Manuela Alecio <manuelaalecio.dev@gmail.com>
2022-12-01 08:16:24 -0300

    add sound to all the buttons, and it change color

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ const forestSoundOff = document.querySelector('#forestSoundOff')
 const forestSoundOn = document.querySelector('#forestSoundOn')
 const rainSoundOff = document.querySelector('#rainSoundOff')
 const rainSoundOn = document.querySelector('#rainSoundOn')
+const storeSoundOff = document.querySelector('#storeSoundOff')
+const storeSoundOn = document.querySelector('#storeSoundOn')
+const fireSoundOff = document.querySelector('#fireSoundOff')
+const fireSoundOn = document.querySelector('#fireSoundOn')
 
 const forestSound = new Audio('../sounds/forest.wav')
 const rainSound = new Audio('../sounds/rain.wav')
@@ -20,9 +24,38 @@ forestSoundOn.addEventListener('click', () => {
   forestSound.pause()
 })
 
-rainSoundOff.addEventListener('click', function() {
-  // rainSoundOff.classList.toggle('hide')
-  // rainSoundOn.classList.toggle('hide')
-  // rainSound.play()
-  console.log('estado')
+rainSoundOff.addEventListener('click', () => {
+  rainSoundOff.classList.toggle('hide')
+  rainSoundOn.classList.toggle('hide')
+  rainSound.play()
+})
+
+rainSoundOn.addEventListener('click', () => {
+  rainSoundOff.classList.toggle('hide')
+  rainSoundOn.classList.toggle('hide')
+  rainSound.pause()
+})
+
+storeSoundOff.addEventListener('click', () => {
+  storeSoundOff.classList.toggle('hide')
+  storeSoundOn.classList.toggle('hide')
+  storeSound.play()
+})
+
+storeSoundOn.addEventListener('click', () => {
+  storeSoundOff.classList.toggle('hide')
+  storeSoundOn.classList.toggle('hide')
+  storeSound.pause()
+})
+
+fireSoundOff.addEventListener('click', () => {
+  fireSoundOff.classList.toggle('hide')
+  fireSoundOn.classList.toggle('hide')
+  fireSound.play()
+})
+
+fireSoundOn.addEventListener('click', () => {
+  fireSoundOff.classList.toggle('hide')
+  fireSoundOn.classList.toggle('hide')
+  fireSound.pause()
 })
